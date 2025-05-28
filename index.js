dd661cb7bea42a23e0414a04a0b1daa07b5055b1
Deeja-ish <khadijaismail696@gmail.com>
2025-05-28 11:06:45 +0100

    some corrections made

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbxmzxLCV44MVuo06XWGkv4olbq2lh_bWHcvvdbM78F86Oq2XyPXxU5NQuqSBKDnYj9OWw/exec"
+const scriptURL = "https://script.google.com/macros/s/AKfycby9q5raiPrBFL-QnFbjyoXfe4ZRqCrTBT2OOBx_Mpmj2z580HpAsubt2lduJOtYVitddA/exec"
+const form = document.forms['submit-to-google-sheet']
+const successMessage = document.getElementById("message");
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        console.log('Success!', response)
-        alert("Thank you for signing up! We will keep you updated.")
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-});
+  e.preventDefault()
+
+  successMessage.innerHTML = 'Submitting....'
+
+  fetch(scriptURL,{ 
+    method: 'POST', 
+    body: new FormData(form)
+  })
+    // start editting
+  .then(response => {
+
+    if(!response.ok){
+      return response.text().then(text => { throw new Error(text) })
+    }
+    return response.json();
+  })
+
+  .then(data => {
+
+    if(data.result === 'success'){
+      successMessage.innerHTML = "Thank you for Signing up! We will keep you updated.";
+      console.log('Success from App script:', data)
+      form.reset();
+    }
+    else{
+      successMessage.innerHTML = "Error: " + data.Error;
+      successMessage.style.color = 'red'
+      console.error('Error from App Script:', data.error);
+    }
+    setTimeout(function() {
+
+      successMessage.innerHTML = "";
+      successMessage.style.color = "";
+    }, 5000)
+  })
+  .catch(error =>{
+    console.log('Fetch Error!', error)
+    successMessage.innerHTML ='Failed to submit, Please try again'
+    successMessage.style.color = 'red'
+    setTimeout(function(){
+      successMessage.innerHTML = "";
+      successMessage.style.color = "";
+    }, 5000)
+  })
+})
+      // .then(response => {
+      //   successMessage.innerHTML = "Thank you for Signing up! We will keep you updated.";
+      //   setTimeout(function () {
+      //     // Clear the success message after 5 seconds
+      //     successMessage.innerHTML = "";
+      //   }, 5000);
+      //   console.log('Success!', response)
+      //   // Reset the form after successful submission
+      //   form.reset()
+      // })
+      // .catch(error => console.error('Error!', error.message))
+
+
